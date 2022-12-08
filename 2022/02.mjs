d4758e34cf0203d6fb8ca6a3314a2bb648411c0a
Andrew Bruner <andrewbruner@gmail.com>
2022-12-08 03:13:16 +0000

    Refactor to major ternary operation

--- 2022/02.mjs
+++ 2022/02.mjs
@@ -1,115 +1,20 @@
 import input from './02-input.mjs';
 
 /**
- * Used Functions/Methods
- * 
- * parseInt()
- * Array.prototype.at()
- * Array.prototype.map()
- * Array.prototype.reduce()
- * Array.prototype.sort()
- * Array.prototype.split()
- * console.log()
+ * Returns the total score from a Rock-Paper-Scissors encrypted strategy guide
+ * @param {string} input - Raw input string of the strategy guide
+ * @param {boolean} [key=false] - Whether you have the key to the strategy guide
+ * @returns {number} - Total score after following the strategy guide
  */
+function getTotalScore(input, key = false) {
+	const [A,B,X,Y] = ['A','B','X','Y'];
+	return input.split(`\n`).map(el => el.split(' ')).map(el => {
+		let [i,j] = [el[0],el[1]];
+		return i == A ? j == X ? key ? 3 : 4 : j == Y ? key ? 4 : 8 : key ? 8 : 3
+			: i == B ? j == X ? 1 : j == Y ? 5 : 9
+			: j == X ? key ? 2 : 7 : j == Y ? key ? 6 : 2 : key ? 7 : 6;
+	}).reduce((acc, curr) => acc + curr);
+}
 
-/* --- Part One --- */
-// X = Rock
-// Y = Paper
-// Z = Scissors
-
-let tournament = input.split(`\n`)
-tournament = tournament.map(round => round.split(' '));
-let scores = tournament.map(round => {
-	let opponent = round[0];
-	let response = round[1];
-	let shape;
-	let outcome;
-	if (opponent === 'A') {
-		if (response === 'X') {
-			shape = 1;
-			outcome = 3;
-		} else if (response === 'Y') {
-			shape = 2;
-			outcome = 6;
-		} else if (response === 'Z') {
-			shape = 3;
-			outcome = 0;
-		}
-	} else if (opponent === 'B') {
-		if (response === 'X') {
-			shape = 1;
-			outcome = 0;
-		} else if (response === 'Y') {
-			shape = 2;
-			outcome = 3;
-		} else if (response === 'Z') {
-			shape = 3;
-			outcome = 6;
-		}
-	} else if (opponent === 'C') {
-		if (response === 'X') {
-			shape = 1;
-			outcome = 6;
-		} else if (response === 'Y') {
-			shape = 2;
-			outcome = 0;
-		} else if (response === 'Z') {
-			shape = 3;
-			outcome = 3;
-		}
-	}
-	return shape + outcome;
-});
-let totalScore = scores.reduce((total, score) => total + score);
-console.log(totalScore);
-
-/* --- Part Two --- */
-// X = Lose
-// Y = Draw
-// Z = Win
-
-tournament = input.split(`\n`)
-tournament = tournament.map(round => round.split(' '));
-scores = tournament.map(round => {
-	let opponent = round[0];
-	let response = round[1];
-	let shape;
-	let outcome;
-	if (opponent === 'A') {						// Rock
-		if (response === 'X') {					// Lose w/ Scissors
-			shape = 3;
-			outcome = 0;
-		} else if (response === 'Y') {	// Draw w/ Rock
-			shape = 1;
-			outcome = 3;
-		} else if (response === 'Z') {	// Win w/ Paper
-			shape = 2;
-			outcome = 6;
-		}
-	} else if (opponent === 'B') {		// Paper
-		if (response === 'X') {					// Lose w/ Rock
-			shape = 1;
-			outcome = 0;
-		} else if (response === 'Y') {	// Draw w/ Paper
-			shape = 2;
-			outcome = 3;
-		} else if (response === 'Z') {	// Win w/ Scissors
-			shape = 3;
-			outcome = 6;
-		}
-	} else if (opponent === 'C') {		// Scissors
-		if (response === 'X') {					// Lose w/ Paper
-			shape = 2;
-			outcome = 0;
-		} else if (response === 'Y') {	// Draw w/ Scissors
-			shape = 3;
-			outcome = 3;
-		} else if (response === 'Z') {	// Win w/ Rock
-			shape = 1;
-			outcome = 6;
-		}
-	}
-	return shape + outcome;
-});
-totalScore = scores.reduce((total, score) => total + score);
-console.log(totalScore);
+console.log(getTotalScore(input));
+console.log(getTotalScore(input, true));
