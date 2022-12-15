a762cc2d27929e055c35c27db3b576b284929d8a
Andrew Bruner <andrewbruner@gmail.com>
2022-12-15 13:49:59 +0000

    Add Day 6 file

---
+++ 2022/06.js
@@ -0,0 +1,14 @@
+// device
+// communication system
+// lock on to signal
+// start-of-packet marker in datastream
+// four different chars (protocol)
+
+// start-of-message in datastream
+// 14 distinct characters
+
+import Input from './input/06.js';
+
+let input = Input;
+
+console.log(input);
