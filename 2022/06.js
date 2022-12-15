811e0016cdb6e833a6e106754dd41fdcf30dc709
Andrew Bruner <andrewbruner@gmail.com>
2022-12-15 19:34:58 +0000

    Solve for Day 6

--- 2022/06.js
+++ 2022/06.js
@@ -9,6 +9,37 @@
 
 import Input from './input/06.js';
 
-let input = Input;
+const MarkerLength = 4;
 
-console.log(input);
+let datastream = 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw';
+
+function checkDatastream(datastream, options) {
+	let markerLength = options.markerLength;
+	dataLoop:
+		for (let i = 0; i < datastream.length; i++) {
+			let marker = datastream.substring(i, i + markerLength);
+			markerLoop:
+				for (let j = 1; j < marker.length; j++) {
+					let char = marker[j];
+					let substr = marker.substring(0, j);
+					let isLastIdx = j === marker.length - 1;
+					if (substr.includes(char)) break markerLoop;
+					if (isLastIdx) return i + j + 1;
+				}
+		}
+}
+
+console.log(checkDatastream(Input, { markerLength: 14 }));
+
+// // check if marker position is distinct
+// if (signal.isDistinct) console.log(signal.position);
+// // check for marker distinction with remaining signal characters
+// for (let i = 0; i < datastream.length - MarkerLength; i++) {
+// 	if (!signal.isDistinct) {
+// 		let char = datastream[i + MarkerLength];
+// 		let substr = datastream.substring(i + 1, i + MarkerLength);
+// 		signal = { ...signal, position: i + MarkerLength + 1};
+// 		console.log(substr, char);
+// 		if (!substr.includes(char)) signal = { ...signal, isDistinct: true };
+// 	}
+// }
