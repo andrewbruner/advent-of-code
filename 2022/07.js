9171d66ff434f25310e4f95db01ce1b525b3aae6
Andrew Bruner <andrewbruner@gmail.com>
2022-12-16 00:10:16 +0000

    Solve for Day 7

--- 2022/07.js
+++ 2022/07.js
@@ -1,121 +1,126 @@
-import input from './input/07.js';
-// let input = `$ cd /
-// $ ls
-// dir a
-// 14848514 b.txt
-// 8504156 c.dat
-// dir d
-// $ cd a
-// $ ls
-// dir e
-// 29116 f
-// 2557 g
-// 62596 h.lst
-// $ cd e
-// $ ls
-// 584 i
-// $ cd ..
-// $ cd ..
-// $ cd d
-// $ ls
-// 4060174 j
-// 8033020 d.log
-// 5626152 d.ext
-// 7214296 k`;
+import Input from './input/07.js';
+let input = `$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k`
 
-// navigation object with current location
-let nav = {
-	loc: '/',
-};
+function File(name, size, parentDir) {
+	this.name = name;
+	this.getSize = getSize;
 
-// references to all directories
-let refs = {
-	'/': { info: { type: 'dir', path: '/', size: 0 } },
-};
+	parentDir.contents.push(this);
 
-// filesystem object
-let fs = {
-	'/': refs['/'],
-};
-
-
-
-// split input log into separate lines
-let log = input.split('\n');
-// split each line into separate components
-log = log.map(ln => ln.split(' '));
-
-log.forEach(ln => {
-	if (ln[0] === '$') {
-		if (ln[1] === 'cd') {
-			if (ln[2] === '/') {
-				nav.loc = '/';
-			} else if (ln[2] === '..') {
-				nav.loc = nav.loc.split('/');
-				nav.loc = nav.loc.slice(0,nav.loc.length-1);
-				nav.loc = nav.loc.join('/')
-			} else {
-				nav.loc = nav.loc += `/${ln[2]}`;
-			}
-		}
-	} else if (ln[0] === 'dir') {
-		let type = 'dir';
-		let path = `${nav.loc}/${ln[1]}`;
-		let size = 0;
-		let dir = { info: { type, path, size } };
-		refs[path] = dir;
-		refs[nav.loc][path] = dir;
-	} else {
-		let type = 'file';
-		let path = `${nav.loc}/${ln[1]}`
-		let size = parseInt(ln[0]);
-		let file = { info: { type, path, size } };
-		refs[path] = file;
-		refs[nav.loc][path] = file;
+	function getSize() {
+		return size;
 	}
-});
+}
 
-function size(type) {
-	let depth = 0;
-	for (let ref in refs) {
-		let curr = refs[ref].info.path.split('/').length;
-		if (curr > depth) {
-			depth = curr;
-		}
+function Dir(name, parentDir) {
+	this.isDir = true;
+	this.parentDir = parentDir;
+	this.name = name;
+	this.getSize = getSize;
+	this.contents = [];
+
+	if (parentDir) {
+		parentDir.contents.push(this);
 	}
-	while (depth >= 0) {
-		for (let ref in refs) {
-			if (refs[ref].info.type === type && ref !== '/') {
-				let parent = refs[ref].info.path.split('/');
-				if (parent.length === depth) {
-					parent = parent.slice(0,parent.length-1);
-					parent = parent.join('/');
-					refs[parent].info.size += refs[ref].info.size;
+
+	function getSize() {
+		return this.contents.length ? this.contents.map(v => v.getSize()).reduce((acc, curr) => acc + curr) : 0;
+	}
+}
+
+function Filesystem(input) {
+	this.rootDir = new Dir('/', null);
+	this.build = build;
+	
+	function build() {
+		let log = input.split('\n').map(ln => ln.split(' '));
+		let currDir = this.rootDir;
+		log.forEach(ln => {
+			if (ln[0] === '$') {
+				if (ln[1] === 'cd') {
+					let dir = ln[2];
+					if (dir === '/') {
+						currDir = this.rootDir;
+					} else if (dir === '..') {
+						currDir = currDir.parentDir;
+					} else {
+						currDir = currDir.contents.find(v => v.isDir && v.name === dir);
+					}
 				}
+			} else if (ln[0] === 'dir') {
+				let name = ln[1];
+				new Dir(name, currDir);
+			} else {
+				let size = parseInt(ln[0]);
+				let name = ln[1];
+				new File(name, size, currDir);
 			}
+		});
+		return this;
+	}
+}
+
+let filesystem = new Filesystem(Input).build();
+
+let sum = 0;
+
+function addSum(item) {
+	if (item.isDir) {
+		item.contents.forEach(v => addSum(v));
+		if (item.getSize() <= 100000) {
+			sum += item.getSize();
 		}
-		depth--;
 	}
 }
 
-size('file');
-size('dir');
+addSum(filesystem.rootDir);
 
-// console.log('FILESYSTEM');
-// console.dir(fs, { depth: null });
-// console.log('REFERENCE');
-// console.dir(refs);
 
-console.log(fs);
+// PART 2
 
-let dirs = [];
-for (let ref in refs) {
-	let size = refs[ref].info.size;
-	if (size <= 100000) {
-		dirs.push(size);
+let rootDir = filesystem.rootDir;
+let totalDiskSpace = 70_000_000;
+let currInUse = filesystem.rootDir.getSize(); // 43_956_976
+let currAvailable = totalDiskSpace - currInUse; // 26_043_024
+let targetAvailable = 30_000_000;
+let targetToDelete = targetAvailable - currAvailable; // 3_956_976
+
+
+let candidates = [];
+function getCandidates(v) {
+	if (v.isDir) {
+		v.contents.forEach(v => getCandidates(v));
+		if (v.getSize() >= targetToDelete) {
+			candidates.push(v);
+		}
 	}
 }
+getCandidates(rootDir);
 
-dirs.forEach(dir => console.log(dir));
-dirs = dirs.reduce((acc, curr) => acc + curr);
-console.log(dirs);
+let sizes = candidates.map(v => v.getSize());
+
+let smallest = Math.min(...sizes);
+
+console.log(smallest);
