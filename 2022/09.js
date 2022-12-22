5f92a3a814986a97654a237f9f81c7ae4486056f
Andrew Bruner <andrewbruner@gmail.com>
2022-12-22 03:57:51 +0000

    Refactor and Solve for Day 9 (part 2)

--- 2022/09.js
+++ 2022/09.js
@@ -11,84 +11,220 @@ R 2`;
 
 let motions = Input.split('\n')
 	.map(motion => motion.split(' '))
-	.map(motion => [motion[0], parseInt(motion[1])]);
+	.map(motion => ({ dir: motion[0], dist: parseInt(motion[1]) }));
 
 console.log('MOTIONS');
 console.log(motions);
 
-function Rope() {
-	this.head = { x: 0, y: 0 };
-	this.tail = { x: 0, y: 0 };
-	this.moveHead = moveHead;
-	this.moveTail = moveTail;
-	this.visited = [[0, 0]];
-
-	function moveHead(motion) {
-		let dir = motion[0];
-		let dist = motion[1];
-
-		if (dir === 'U') {
-			for (let i = 0; i < dist; i++) {
-				this.head.y++
-				this.moveTail(dir);
-			}
-		}
-		if (dir === 'R') {
-			for (let i = 0; i < dist; i++) {
-				this.head.x++
-				this.moveTail(dir);
-			}
-		}
-		if (dir === 'D') {
-			for (let i = 0; i < dist; i++) {
-				this.head.y--
-				this.moveTail(dir);
-			}
-		}
-		if (dir === 'L') {
-			for (let i = 0; i < dist; i++) {
-				this.head.x--
-				this.moveTail(dir);
-			}
-		}
+class Knot {
+	constructor() {
+		this.x = 0;
+		this.y = 0;
+		this.next = null;
 	}
+}
 
-	function moveTail(dir) {
-		if (dir === 'U') {
-			if (this.head.y > this.tail.y + 1) {
-				this.tail.x = this.head.x;
-				this.tail.y = this.head.y - 1;
-				this.visited.push([this.tail.x, this.tail.y])
+let head = new Knot();
+let tail = new Knot();
+head.next = tail;
+let rope = { head, tail, };
+let numKnots = 10;
+for (let i = 2; i < numKnots; i++) {
+	let knot = new Knot();
+	rope.tail.next = knot;
+	rope.tail = knot;
+}
+let visitedByTail = ['0,0'];
+
+function moveNext(knot) {
+	if (knot.next) {
+		if (knot.next.x === knot.x) {
+			if (knot.next.y < knot.y-1) {
+				knot.next.y++;
+				moveNext(knot.next);
+			} else if (knot.next.y > knot.y+1) {
+				knot.next.y--;
+				moveNext(knot.next);
 			}
+		} else if (knot.next.y === knot.y) {
+			if (knot.next.x < knot.x-1) {
+				knot.next.x++;
+				moveNext(knot.next);
+			} else if (knot.next.x > knot.x+1) {
+				knot.next.x--;
+				moveNext(knot.next);
+			}
+		} else if (
+			(knot.next.x < knot.x && knot.next.y < knot.y-1)
+			|| (knot.next.x < knot.x-1 && knot.next.y < knot.y)
+		) {
+			knot.next.x++;
+			knot.next.y++;
+			moveNext(knot.next);
+		} else if (
+			((knot.next.x < knot.x-1 && knot.next.y > knot.y)
+			|| (knot.next.x < knot.x && knot.next.y > knot.y+1))
+		) {
+			knot.next.x++;
+			knot.next.y--;
+			moveNext(knot.next);
+		} else if (
+			(knot.next.x > knot.x && knot.next.y > knot.y+1)
+			|| (knot.next.x > knot.x+1 && knot.next.y > knot.y)
+		) {
+			knot.next.x--;
+			knot.next.y--;
+			moveNext(knot.next);
+		} else if (
+			(knot.next.x > knot.x+1 && knot.next.y < knot.y)
+			|| (knot.next.x > knot.x && knot.next.y < knot.y-1)
+		) {
+			knot.next.x--;
+			knot.next.y++;
+			moveNext(knot.next);
 		}
-		if (dir === 'R') {
-			if (this.head.x > this.tail.x + 1) {
-				this.tail.x = this.head.x - 1;
-				this.tail.y = this.head.y;
-				this.visited.push([this.tail.x, this.tail.y])
-			}
-		}
-		if (dir === 'D') {
-			if (this.head.y < this.tail.y - 1) {
-				this.tail.x = this.head.x;
-				this.tail.y = this.head.y + 1;
-				this.visited.push([this.tail.x, this.tail.y])
-			}
-		}
-		if (dir === 'L') {
-			if (this.head.x < this.tail.x - 1) {
-				this.tail.x = this.head.x + 1;
-				this.tail.y = this.head.y;
-				this.visited.push([this.tail.x, this.tail.y])
-			}
+		if (!knot.next.next) {
+			visitedByTail = [ ...visitedByTail, `${knot.next.x},${knot.next.y}`, ];
 		}
 	}
 }
 
-let rope = new Rope();
+motions.forEach(motion => {
+	if (motion.dir === 'U') {
+		for (let i = 0; i < motion.dist; i++) {
+			rope.head.y++;
+			moveNext(rope.head);
+		}
+	} else if (motion.dir === 'R') {
+		for (let i = 0; i < motion.dist; i++) {
+			rope.head.x++;
+			moveNext(rope.head);
+		}
+	} else if (motion.dir === 'D') {
+		for (let i = 0; i < motion.dist; i++) {
+			rope.head.y--;
+			moveNext(rope.head);
+		}
+	} else if (motion.dir === 'L') {
+		for (let i = 0; i < motion.dist; i++) {
+			rope.head.x--;
+			moveNext(rope.head);
+		}
+	}
+});
 
-motions.forEach(motion => rope.moveHead(motion));
-rope.visited = rope.visited.map(pos => `${pos[0]},${pos[1]}`);
-rope.visited = rope.visited.filter((pos, idx, arr) => arr.indexOf(pos) === idx);
-console.log('visited', rope.visited);
-console.log('unique visited', rope.visited.length);
+visitedByTail = visitedByTail.filter((pos, idx, arr) => arr.indexOf(pos) === idx);;
+
+console.log(visitedByTail.length);
+
+// class Rope {
+// 	constructor() {
+// 		this.head = null;
+// 		this.tail = null;
+// 		this.visitedTailPositions = [];
+// 	},
+// 	moveHead(motion) {
+
+// 	}
+// 	followHead(prevKnot) {}
+// }
+
+// class Knot {
+// 	constructor(prev, next = null) {
+// 		this.prev = prev;
+// 		this.next = next;
+// 		this.coords = { x: 0, y: 0 };
+// 	}
+// }
+
+// function buildRope({ numKnots = 2 } = { }) {
+// 	let rope = new Rope();
+// 	let head = new Knot(null);
+// 	rope.head = head;
+// 	rope.tail = head;
+// 	let prevKnot = head;
+// 	for (let i = 1; i < numKnots; i++)  {
+// 		let nextKnot = new Knot(prevKnot);
+// 		prevKnot.next = nextKnot;
+// 		rope.tail = nextKnot;
+// 		prevKnot = nextKnot;
+// 	}
+// 	return rope;
+// }
+
+// let rope = buildRope({ numKnots: 3 });
+// console.dir(rope, { depth: null });
+
+// // 	this.moveHead = moveHead;
+// // 	this.moveTail = moveTail;
+// // 	this.visited = [[0, 0]];
+
+// // 	function moveHead(motion) {
+// // 		let dir = motion[0];
+// // 		let dist = motion[1];
+
+// // 		if (dir === 'U') {
+// // 			for (let i = 0; i < dist; i++) {
+// // 				this.head.y++
+// // 				this.moveTail(dir);
+// // 			}
+// // 		}
+// // 		if (dir === 'R') {
+// // 			for (let i = 0; i < dist; i++) {
+// // 				this.head.x++
+// // 				this.moveTail(dir);
+// // 			}
+// // 		}
+// // 		if (dir === 'D') {
+// // 			for (let i = 0; i < dist; i++) {
+// // 				this.head.y--
+// // 				this.moveTail(dir);
+// // 			}
+// // 		}
+// // 		if (dir === 'L') {
+// // 			for (let i = 0; i < dist; i++) {
+// // 				this.head.x--
+// // 				this.moveTail(dir);
+// // 			}
+// // 		}
+// // 	}
+
+// // 	function moveTail(dir) {
+// // 		if (dir === 'U') {
+// // 			if (this.head.y > this.tail.y + 1) {
+// // 				this.tail.x = this.head.x;
+// // 				this.tail.y = this.head.y - 1;
+// // 				this.visited.push([this.tail.x, this.tail.y])
+// // 			}
+// // 		}
+// // 		if (dir === 'R') {
+// // 			if (this.head.x > this.tail.x + 1) {
+// // 				this.tail.x = this.head.x - 1;
+// // 				this.tail.y = this.head.y;
+// // 				this.visited.push([this.tail.x, this.tail.y])
+// // 			}
+// // 		}
+// // 		if (dir === 'D') {
+// // 			if (this.head.y < this.tail.y - 1) {
+// // 				this.tail.x = this.head.x;
+// // 				this.tail.y = this.head.y + 1;
+// // 				this.visited.push([this.tail.x, this.tail.y])
+// // 			}
+// // 		}
+// // 		if (dir === 'L') {
+// // 			if (this.head.x < this.tail.x - 1) {
+// // 				this.tail.x = this.head.x + 1;
+// // 				this.tail.y = this.head.y;
+// // 				this.visited.push([this.tail.x, this.tail.y])
+// // 			}
+// // 		}
+// // 	}
+// // }
+
+// // let rope = new Rope();
+
+// // motions.forEach(motion => rope.moveHead(motion));
+// // rope.visited = rope.visited.map(pos => `${pos[0]},${pos[1]}`);
+// // rope.visited = rope.visited.filter((pos, idx, arr) => arr.indexOf(pos) === idx);
+// // console.log('visited', rope.visited);
+// // console.log('unique visited', rope.visited.length);
