464f37f6c88f01663021a1f00d9939372399de60
Andrew Bruner <andrewbruner@gmail.com>
2022-12-23 00:53:14 +0000

    Begin mutation of input to array of objects

--- 2022/11.js
+++ 2022/11.js
@@ -1,3 +1,49 @@
+let input = `Monkey 0:
+Starting items: 79, 98
+Operation: new = old * 19
+Test: divisible by 23
+	If true: throw to monkey 2
+	If false: throw to monkey 3
+
+Monkey 1:
+Starting items: 54, 65, 75, 74
+Operation: new = old + 6
+Test: divisible by 19
+	If true: throw to monkey 2
+	If false: throw to monkey 0
+
+Monkey 2:
+Starting items: 79, 60, 97
+Operation: new = old * old
+Test: divisible by 13
+	If true: throw to monkey 1
+	If false: throw to monkey 3
+
+Monkey 3:
+Starting items: 74
+Operation: new = old + 3
+Test: divisible by 17
+	If true: throw to monkey 0
+	If false: throw to monkey 1`;
+
+input = input.split('\n\n')
+	.map(monkey => monkey.split('\n'))
+	.map(monkey => {
+		let items = monkey[1].split(':')
+			[1].split(', ')
+			.map(v => +v);
+			
+		return {
+			items,
+			operation: monkey[2],
+			test: monkey[3],
+			true: monkey[4],
+			false: monkey[5],
+		}
+	});
+
+console.dir(input);
+
 let monkeys = [
 	{
 		items: [ 79, 98, ],
@@ -48,7 +94,5 @@ inspections.forEach(num => {
 	}
 });
 
-console.log(inspections);
-console.log(max, nextMax);
 let monkeyBusiness = max * nextMax;
 console.log(monkeyBusiness);
