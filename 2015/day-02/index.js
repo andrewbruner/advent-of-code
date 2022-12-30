bfa47ba0e03fcde182e65d6194e425cf8b754c03
Andrew Bruner <andrewbruner@gmail.com>
2022-12-30 16:52:51 +0000

    Solve for Day 2 (part 2)

--- 2015/day-02/index.js
+++ 2015/day-02/index.js
@@ -4,16 +4,30 @@ function dayTwo(input) {
 	input = input.split('\n')
 		.map(dims => {
 			dims = dims.split('x');
+			// measurements
 			const l = +dims[0];
 			const w = +dims[1];
 			const h = +dims[2];
-			const sides = [ l * w, w * h, h * l, ];
-			const area = 2 * sides[0] + 2 * sides[1] + 2 * sides[2];
-			const slack = Math.min(...sides);
-			const sum = area + slack;
-			return sum;
+			const areas = [ l * w, w * h, h * l, ];
+			const perims = [ 2 * l + 2 * w, 2 * w + 2 * h, 2 * h + 2 * l ];
+			const vol = l * w * h;
+			// paper
+			const paperWrapping = 2 * areas[0] + 2 * areas[1] + 2 * areas[2];
+			const paperSlack = Math.min(...areas);
+			const paper = paperWrapping + paperSlack;
+			// ribbon
+			const ribbonWrapping = Math.min(...perims);
+			const ribbonBow = vol;
+			const ribbon = ribbonWrapping + ribbonBow;
+			const wrapping = { paper, ribbon };
+			return wrapping;
 		})
-		.reduce((acc, curr) => curr + acc);
+		.reduce((acc, curr) => {
+			return {
+				paper: curr.paper + acc.paper,
+				ribbon: curr.ribbon + acc.ribbon,
+			};
+		});
 	return input;
 }
 
